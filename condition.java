994f9d6c8f99026e86095202e9d687c292c336f3
harshgaikwad12 <142099995+harshgaikwad12@users.noreply.github.com>
2023-08-19 19:02:11 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,12 @@
+public class condition 
+{
+     public static void main(String args[])
+     {
+          boolean x= true;
+          boolean y;
+          y=(x==false)?true:false;
+          System.out.println(y);
+          y=(x==true)?true:false;
+          System.out.println(y);
+     }
+}
