2fd70b3a21b4ee8176babe335edc8b335231a04b
harshgaikwad12 <142099995+harshgaikwad12@users.noreply.github.com>
2023-08-19 19:02:56 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,18 @@
+public class arithmatic
+ {
+     int c=1;
+     int d=2;
+   float e= c+d;     
+     public static void main(String args[])
+     {
+          double a=10;
+          double b=12;
+          System.out.println(a+b);
+          System.out.println(a*b);
+          System.out.println(a/b);
+          System.out.println(a%b);
+          System.out.println(a-b);
+          arithmatic f=new arithmatic();
+          System.out.println(f.e);
+     }
+}
